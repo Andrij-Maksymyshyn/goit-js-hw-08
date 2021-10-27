e34aa2d942c258d29671f728c89aac2aeaf40015
Andrij-Maksymyshyn <84930890+Andrij-Maksymyshyn@users.noreply.github.com>
2021-10-27 17:43:02 +0300

    correct task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,71 +1,38 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 
-import SimpleLightbox from "simplelightbox";
-
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
+        import SimpleLightbox from "simplelightbox";   
+        import 'simplelightbox/dist/simple-lightbox.min.css';
+       
 const galleryPictures = document.querySelector('.gallery');
 const itemsMarkup = createItemsMarkup (galleryItems);
 
 galleryPictures.insertAdjacentHTML('beforeend', itemsMarkup);
 
-galleryPictures.addEventListener('click', onGalleryPicturesClick);
 
+ galleryPictures.addEventListener('click', onGalleryPicturesClick)
 
 function createItemsMarkup (galleryItems) {
     return galleryItems.map(({preview, original, description}) => {
         return `
-        <div class="gallery__item">
-         <a class="gallery__link" href="${original}">
-        <img
-          class="gallery__image"
-          src="${preview}"
-          data-source="${original}"
-          alt="${description}"
-        />
-        </a>
-    </div>
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+</a>
         `;
     })
     .join('');
         }
 
+                function onGalleryPicturesClick (evt) {
+            evt.preventDefault();
+                if (!evt.target.classList.contains("gallery__image")) {
+                return;
+                   }                   
+                  
+                   let lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });                 
+            
+                        }
 
-        import * as basicLightbox from 'basiclightbox'
-        
-        let instance = basicLightbox.create(
-            `<img src="" />`, {
-
-            onShow: instance => {
-                window.addEventListener('keydown', pressKey);
-            },
-            onClose: instance => {
-                window.removeEventListener('keydown', pressKey);
-            },
-        });
-
-        function pressKey(event) {
-                        if (event.code === 'Escape') {
-                instance.close();
-                          }
-        }
-
-       
-function onGalleryPicturesClick (evt) {
-    evt.preventDefault();
-        if (!evt.target.classList.contains("gallery__image")) {
-        return;
-           }
-
-          instance.element().querySelector('img').src = evt.target.dataset.source; 
-        instance.show();
-
-        instance.element().querySelector('img').addEventListener("click", () => {
-            instance.close();
-        }, {once: true});
-                                       
-    }
 // Change code below this line
 
 
